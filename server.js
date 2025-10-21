21a50b1439c81cd103396e77eb759bda9aa0d7eb
Hamman Afakirya <114332800+Afaks-11@users.noreply.github.com>
2025-10-21 19:04:01 +0100

    Remove duplicate CORS middleware usage

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
 
 app.get('/', (req,res) => { 
   res.json({ message: 'Hello world'})})
